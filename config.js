02af261fe7ca4d78be5f4bc052c3aaac09867f66
Andy Gardner <gardnerandy@gmail.com>
2022-04-19 15:14:53 +0000

    Updates for dockerization

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 const config = {
   db: {
     /* don't expose password or any sensitive info, done only for demo */
-    host: "localhost",
+    host: "157.230.9.21",
     user: "eekhoorntje",
     password: "mysqlpass",
     database: "running",
+    port: 3306
   },
-  listPerPage: 10,
+  listPerPage: 20,
 };
 module.exports = config;
