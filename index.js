bf021914f7e96d6218e31060f8be82b450ca05b9
Gardner <ag012418@cerner.net>
2022-04-19 20:45:41 -0500

    Adding Garmin Activity get route and logic

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const programmingLanguagesRouter = require("./routes/programmingLanguages");
+const garminActivitiesRouter = require("./routes/garminActivities");
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -12,6 +13,7 @@ app.get("/", (req, res) => {
   res.json({ message: "ok" });
 });
 app.use("/programming-languages", programmingLanguagesRouter);
+app.use("/garmin-activities", garminActivitiesRouter);
 /* Error handler middleware */
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
